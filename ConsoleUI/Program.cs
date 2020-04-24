95ef899d9f3dd59f24e23ebf3011c030a3bcc408
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:51:23 -0700

    Initial Import

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            for (int i = 0; i < 100; i++)
+            {
+                count *= 5;
+                Console.WriteLine($"The value of count is { count }");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
